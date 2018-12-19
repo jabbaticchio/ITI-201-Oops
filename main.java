7b62e15fa7eb02a3739d50fc9e5914611ac43d1b
Gaurav Shirodkar <coolgauru@gmail.com>
2018-12-18 22:25:05 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to OOPS. ");/// WRITE SOME MORE EXPOSITION HERE PLZZZZZZ
+    
+    
+    StartGame();
+
+
+
+    
+  }
+}
