6e426617f4fa4a3c94fd290f4e3e05a9832b7ceb
Gaurav Shirodkar <coolgauru@gmail.com>
2018-12-20 03:20:21 -0500

    Update HillCenterPath.java

--- HillCenterPath.java
+++ HillCenterPath.java
@@ -1,14 +1,20 @@
 import java.util.*;
-public class HillCenterPath{
-	public static void main (String[]args) {
+public class HillCenterPath extends Room{
+	int[] possibleoutputindices = {0,9} ;
+	@Override
+	public void handle() {
 		Scanner scan=new Scanner (System.in);
 		Random randgen=new Random();
 		boolean valid=true;
 		System.out.println("You walked to the Hill Center Bus Stop knowing that this stop was the best /n method to get back to College Ave for the exam!");
 		System.out.println("The stop isn't very crowded but, you may need to wait.");
-		while (true) {
+		
+		
+
+		
 		if (randgen.nextInt(10)==0) {
 			System.out.println("Awesome! You got the bus immediately! Now Taking the bus to college avenue student center...");// You got lucky and took the bus to college ave
+			
 		}else {
 				System.out.println("You thought the bus would come in a minute, but it was completely full and no one got on on. Wait another 15 minutes for the next bus?");
 				System.out.println("Or, will you cut your losses and go back to the SERC Fields? (Type 1 to stay or 2 to go back.");
@@ -16,13 +22,15 @@ public class HillCenterPath{
 				while(valid){
 					if (decision == 1)//wait for bus
 					{
-						valid = false;
+						
 						System.out.println("You decided to stay and wait for the next bus.");
+						valid = false;
 					}
 					else if(decision == 2)//go back to serc fields
 					{
-						valid = false;
+						
 						System.out.println("You decided to go back to the SERC Fields");
+						valid = false;
 					}
 					else
 					{
@@ -35,7 +43,7 @@ public class HillCenterPath{
 				
 				
 				
-			}
+			
 			
 		}
 		}
