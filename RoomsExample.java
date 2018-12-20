8d5155e5d1c0736a07b05c2b274a90810544f17a
Gaurav Shirodkar <coolgauru@gmail.com>
2018-12-20 02:24:13 -0500

    Update RoomsExample.java

--- RoomsExample.java
+++ RoomsExample.java
@@ -6,44 +6,44 @@ public class RoomsExample {
 		Random rd = new Random();
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Room> myRooms=new ArrayList<Room>();
-		Room SercFields=new SercFields("Serc Fields",availableMoves, int randomNumber);
+		Room SercFields=new SercFields("Serc Fields",availableMoves);
 		myRooms.add(SercFields);
-		Room ARC=new ARC("Alison Road Classroom Building",availableMoves, int randomNumber);
+		Room ARC=new ARC("Alison Road Classroom Building",availableMoves);
 		myRooms.add(ARC);
-		Room ChemLabs=new ChemLabs("Chemistry Labs",availableMoves, int randomNumber);
+		Room ChemLabs=new ChemLabs("Chemistry Labs",availableMoves);
 		myRooms.add(ChemLabs);
-		Room BSC=new BSC("Busch Student Center",availableMoves, int randomNumber);
+		Room BSC=new BSC("Busch Student Center",availableMoves);
 		myRooms.add(BSC);
-		Room HCBS=new HCBS("Hill Center Bus Stop",availableMoves, int randomNumber);
+		Room HCBS=new HCBS("Hill Center Bus Stop",availableMoves);
 		myRooms.add(HCBS);
-		Room Woodys=new Woodys("Woodys Food",availableMoves, int randomNumber);
+		Room Woodys=new Woodys("Woodys Food",availableMoves);
 		myRooms.add(Woodys);
-		Room PaneraBSC=new PaneraBSC("Panera Bread at the Busch Student Center",availableMoves, int randomNumber);
+		Room PaneraBSC=new PaneraBSC("Panera Bread at the Busch Student Center",availableMoves);
 		myRooms.add(PaneraBSC);
-		Room GerlandasBSC =new GerlandasBSC("Gerlandas Pizza at the Busch Student Center",availableMoves, int randomNumber);
+		Room GerlandasBSC =new GerlandasBSC("Gerlandas Pizza at the Busch Student Center",availableMoves);
 		myRooms.add(GerlandasBSC);
-		Room MoesBSC=new MoesBSC("Moes at the Busch Student Center" ,availableMoves, int randomNumber);
+		Room MoesBSC=new MoesBSC("Moes at the Busch Student Center" ,availableMoves);
 		myRooms.add(MoesBSC);
-		Room CASC=new CASC("College Avenue Student Center",availableMoves, int randomNumber);
+		Room CASC=new CASC("College Avenue Student Center",availableMoves);
 		myRooms.add(CASC);
-		Room PaneraCASC =new PaneraCASC("Panera Bread at the College Avenue Student Center",availableMoves, int randomNumber);
+		Room PaneraCASC =new PaneraCASC("Panera Bread at the College Avenue Student Center",availableMoves);
 		myRooms.add(PaneraCASC);
-		Room GerlandasCASC=new GerlandasCASC("Gerlandas Pizza at the College Avenue Student Center",availableMoves, int randomNumber);
+		Room GerlandasCASC=new GerlandasCASC("Gerlandas Pizza at the College Avenue Student Center",availableMoves);
 		myRooms.add(GerlandasCASC);
-		Room MoesCASC=new MoesCASC("Moes at the College Avenue Student Center",availableMoves, int randomNumber);
+		Room MoesCASC=new MoesCASC("Moes at the College Avenue Student Center",availableMoves);
 		myRooms.add(MoesCasc);
-		Room OopsRoom= new OopsRoom("Obj Oriented Programming Classroom", availableMoves, int randomNumber);
+		Room OopsRoom= new OopsRoom("Obj Oriented Programming Classroom", availableMoves);
 		myRooms.add(OopsRoom);
 
-		for(int i = 0; i < myRooms.length; i++){
+		for(int i = 0; i < myRooms.size(); i++){
 			String name = "Room " + (i+1);
 			int back = i-1;
 			int forward = i+1;
-			if(i==0) back = myRooms.length-1;
+			if(i==0) back = myRooms.size()-1;
 			if(i==myRooms.length-1) forward = 0;
 			int[] availableMoves = {back, forward};
 			Room tempRoom = new Room(name, availableMoves, rd.nextInt(10));
-			myRooms[i] = tempRoom;
+			myRooms.get(i) = tempRoom;
 		}//end of for loop
 		//for(int i = 0; i < myRooms.length; i++) System.out.println(myRooms[i]);
 		
