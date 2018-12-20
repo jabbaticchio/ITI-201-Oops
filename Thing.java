1f2bd174473c3199097f93d212b740027ad1dc2f
Gaurav Shirodkar <coolgauru@gmail.com>
2018-12-20 01:33:42 -0500

    Create Thing.java

---
+++ Thing.java
@@ -0,0 +1,15 @@
+public class Thing{
+ private String name;
+ private String description;
+ 
+ public void setName(String name){
+ return this.name;}
+ 
+ public void setDescription(String description){
+ this.description=description;
+ }
+ 
+ public String getDescription(){
+ return this.description;
+ }
+ }
