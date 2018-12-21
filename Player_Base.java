3677eea5895596cc6f431ceb1a8298cc9b4b63fe
Gaurav Shirodkar <coolgauru@gmail.com>
2018-12-20 23:54:02 -0500

    Delete Player_Base.java

--- Player_Base.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public abstract class Person_Base {
-	int score;
-	String name;
-	ArrayList backPack = new ArrayList();
-	
-	abstract public int getScore();
-	abstract public void setScore(int x);
-}
